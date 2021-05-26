910ac28aa51917d26876e9e1b4128f7b606a85ea
Marcin Niemira <marcin.niemira@gmail.com>
2021-05-26 19:04:18 +1000

    add basic dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
-
-echo "Num $1"
-echo "regex $2"
-fortuneTeller=false
+set -x
+ID=$1
+ORG=$2
+REPO=$3
+/app/bin/crystal-ball.exe check -n "${ID}" -o "${ORG}" -r "${REPO}"
 echo "::set-output name=fortuneTeller::$fortuneTeller"
