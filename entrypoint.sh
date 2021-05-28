a251947de23bc3597325287f4f1ad07a0879dd9c
Marcin Niemira <marcin.niemira@gmail.com>
2021-05-28 21:40:46 +1000

    bash flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-set -x
+set -xeou pipefail
 
 ID=${1}
 ORG=${2}
 REPO=${3}
 LABELS=${4}
 
-/app/bin/crystal-ball.exe check -n "${ID}" -o "${ORG}" -r "${REPO}" $(echo -n "${LABELS}" | xargs -r -d ',' -IITEM echo -n ' -l "ITEM" ')
+/app/bin/crystal-ball.exe check -n "${ID}" -o "${ORG}" -r "${REPO}" $(echo -n "${LABELS}" | xargs -r -d ',' -IITEM echo -n " -l \"ITEM\"" )
