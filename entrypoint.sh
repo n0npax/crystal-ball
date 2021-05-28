acaabea4b7cbbbe90c9bce99e33c8a20e74e2332
Marcin Niemira <marcin.niemira@gmail.com>
2021-05-28 19:57:59 +1000

    Add license

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ ORG=${2}
 REPO=${3}
 LABELS=${4}
 
-/app/bin/crystal-ball.exe check -n "${ID}" -o "${ORG}" -r "${REPO}" $(echo -n "${LABELS}" | xargs -r -d ',' -IITEM echo -n "-l ITEM ")
+/app/bin/crystal-ball.exe check -n "${ID}" -o "${ORG}" -r "${REPO}" $(echo -n "${LABELS}" | xargs -r -d ',' -IITEM echo -n "'-l ITEM '")
