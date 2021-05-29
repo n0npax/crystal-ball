474f17b9962740a4e4b67226f170dedb3215d5b3
Marcin Niemira <marcin.niemira@gmail.com>
2021-05-29 21:32:58 +1000

    Add test for commenting issue

--- test/check.dart
+++ test/check.dart
@@ -1,15 +1,27 @@
 // Import the test package and Counter class
+import 'package:crystal_ball/check.dart';
+import 'package:crystal_ball/issue.dart';
 import 'package:test/test.dart';
-import '../lib/check.dart';
-import '../lib/issue.dart';
+import 'dart:io';
 
 void main() {
   group('isValidIssue', () {
     test('Empty issue', () async {
       final issue = Issue(org: 'org', repoName: 'repoName', number: '1');
       final reasons = await isValidIssue(issue);
-
       expect(reasons.isNotEmpty, true);
+    });
+  });
+  group('comment invalid issue', () {
+    test('invalid issue #7 issue', () async {
+      final issue = Issue(org: 'n0npax', repoName: 'crystal-ball', number: '7');
+      await issue.init();
+      final currentCommentsCnt = issue.commentsCnt;
+      final testMsg = Platform.environment['GITHUB_REF'] ?? 'test msg';
+      await issue.comment(testMsg);
+      await issue.init();
+      final newCommentsCnt = issue.commentsCnt;
+      expect(newCommentsCnt - 1, equals(currentCommentsCnt));
     });
   });
 }
