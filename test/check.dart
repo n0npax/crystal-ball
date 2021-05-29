4f7120c528082af0333bcc9415f06840a2a6cb25
Marcin Niemira <marcin.niemira@gmail.com>
2021-05-29 21:37:38 +1000

    update test comment msg

--- test/check.dart
+++ test/check.dart
@@ -17,8 +17,8 @@ void main() {
       final issue = Issue(org: 'n0npax', repoName: 'crystal-ball', number: '7');
       await issue.init();
       final currentCommentsCnt = issue.commentsCnt;
-      final testMsg = Platform.environment['GITHUB_REF'] ?? 'test msg';
-      await issue.comment(testMsg);
+      final testMsg = Platform.environment['GITHUB_REF'] ?? 'unknown';
+      await issue.comment('Test comment. Origination ref: $testMsg');
       await issue.init();
       final newCommentsCnt = issue.commentsCnt;
       expect(newCommentsCnt - 1, equals(currentCommentsCnt));
