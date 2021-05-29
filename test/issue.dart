eba505ae9748963b0a569fafbfdddd2006713af5
Marcin Niemira <marcin.niemira@gmail.com>
2021-05-29 21:49:13 +1000

    Add wait to ensure changes are propagated
    improve comment msg

--- test/issue.dart
+++ test/issue.dart
@@ -9,22 +9,28 @@ void main() {
       await issue.init();
       final currentCommentsCnt = issue.commentsCnt;
       final testMsg = Platform.environment['GITHUB_REF'] ?? 'unknown';
-      await issue.comment('Test comment. Origination ref: $testMsg');
+      await issue.comment('Test comment.\`$testMsg`');
       await issue.init();
       final newCommentsCnt = issue.commentsCnt;
       expect(newCommentsCnt - 1, equals(currentCommentsCnt));
     });
     test('label', () async {
+      const foo = 'foo';
+      const bar = 'bar';
       final issue = Issue(org: 'n0npax', repoName: 'crystal-ball', number: '7');
       await issue.init();
       final startLabels = issue.labels;
-      await issue.addLabels(['foo']);
+      // add foo label
+      await issue.addLabels([foo, bar]);
+      await Future.delayed(Duration(seconds: 1));
       await issue.init();
       final updatedLabels = issue.labels;
-      await issue.rmLabels(['foo']);
+      // rm foo label
+      await issue.rmLabels([bar, foo]);
+      await Future.delayed(Duration(seconds: 1));
       await issue.init();
       final finalFabels = issue.labels;
-      expect(updatedLabels.length - 1, startLabels.length);
+      expect(updatedLabels.length - 2, startLabels.length);
       expect(finalFabels.length, startLabels.length);
     });
   });
